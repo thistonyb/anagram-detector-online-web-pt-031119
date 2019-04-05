0e54cd8918e54f7655009e155b46ab88c87cc527
Tony Baker <this.tonybaker@gmail.com>
2019-04-05 00:59:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :new_anagram
+  @@list = []
+
+  def initialize(word)
+    @new_anagram = word
+  end
+
+  def match(words)
+    words.select do |word|
+      an_anagram?(word)
+    end
+  end
+
+  def an_anagram?(word)
+    word.chars.sort == @new_anagram.chars.sort  #Takes a word, turns individual
+  end                                           #characters into an array, sorts
+end                                             #them alphabetically and evaluates.
